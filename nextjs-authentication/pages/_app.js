4bf75400e7612b2ac937a5f25beb3e8f0b1d6aff
Chikku <chikku.george24@gmail.com>
2021-08-23 22:27:37 +0530

    NextAuth Provider

--- nextjs-authentication/pages/_app.js
+++ nextjs-authentication/pages/_app.js
@@ -1,13 +1,14 @@
+import { Provider } from "next-auth/client";
 import Navbar from "../components/Navbar/Navbar";
 import "../styles/globals.css";
 import "../components/Navbar/Navbar.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
