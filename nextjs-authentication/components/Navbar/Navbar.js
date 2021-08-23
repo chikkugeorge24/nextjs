c1307b2d025a43c7528f47e283bed2fcdc7c2471
Chikku <chikku.george24@gmail.com>
2021-08-23 21:56:34 +0530

    NextAuth Client Side Authentication

--- nextjs-authentication/components/Navbar/Navbar.js
+++ nextjs-authentication/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 const Navbar = () => {
+  const [session, loading] = useSession();
   return (
     <nav className="header">
       <h1 className="logo">
         <a href="#">NextAuth</a>
       </h1>
-      <ul className={`main-nav`}>
+      <ul className={`main-nav ${!session && loading ? "loading" : "loaded"}`}>
         <li>
           <Link href="/">
             <a>Home</a>
@@ -24,30 +25,34 @@ const Navbar = () => {
           </Link>
         </li>
 
-        <li>
-          <Link href="/api/auth/signin">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signIn("github");
-              }}
-            >
-              Sign In
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/api/auth/signout">
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              Sign Out
-            </a>
-          </Link>
-        </li>
+        {!session && !loading && (
+          <li>
+            <Link href="/api/auth/signin">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn("github");
+                }}
+              >
+                Sign In
+              </a>
+            </Link>
+          </li>
+        )}
+        {session && (
+          <li>
+            <Link href="/api/auth/signout">
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign Out
+              </a>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
