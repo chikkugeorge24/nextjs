de985164eae9f38c1aba3c0473fb0a012aa03a91
Chikku <chikku.george@reflectionsinfos.com>
2021-08-24 12:58:12 +0530

    Server side authentication

--- nextjs-authentication/pages/_app.js
+++ nextjs-authentication/pages/_app.js
@@ -5,7 +5,7 @@ import "../components/Navbar/Navbar.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider>
+    <Provider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
     </Provider>
--- nextjs-authentication/pages/blog.js
+++ nextjs-authentication/pages/blog.js
@@ -1,7 +1,17 @@
-import React from "react";
+import { getSession } from "next-auth/client";
 
-const blog = () => {
-  return <h1>Blog Page</h1>;
+const blog = ({ data }) => {
+  return <h1>{data}</h1>;
 };
 
 export default blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+      data: session ? "List of 100 personalized blogs" : "List of free blogs",
+    },
+  };
+}
